b74444651b6268b8cf903a823c31bd8a54b48577
imlodinu <lodinu.kalugalage@student.curtin.edu.au>
2023-08-22 17:21:40 +0800

    update project template

--- template/{project}.cpp
+++ template/{project}.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
+#include <fstream>
+#include <vector>
 
-#define IN {project}in.txt
-#define OUT {project}out.txt
+#define IN "{project}in.txt"
+#define OUT "{project}out.txt"
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using i8 = signed char;
+using i16 = signed short;
+using i32 = signed int;
+using i64 = signed long long;
+
+using f32 = float;
+using f64 = double;
+
+template <typename T>
+using vec = std::vector<T>;
 
 int main()
 {
+    std::ifstream in(IN);
+    std::ofstream out(OUT);
+
+    u64 a, b;
+    in >> a >> b;
+
+    out << a + b;
+
     return 0;
 }
